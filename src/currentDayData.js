905debc7a063ddf6102638d06123da7ab867b393
RobertBostrom4 <Robertbostrom4@gmail.com>
2024-01-26 21:06:06 -0700

    Change http to https on API Requests

--- src/currentDayData.js
+++ src/currentDayData.js
@@ -1,5 +1,5 @@
 export async function getCurrentDayData(location) {
-    const weather = await fetch('http://api.weatherapi.com/v1/forecast.json?key=3aa654fe53e441b690942131240801&q=' + location + '&days=1&aqi=no&alerts=no', {mode: "cors"});
+    const weather = await fetch('https://api.weatherapi.com/v1/forecast.json?key=3aa654fe53e441b690942131240801&q=' + location + '&days=1&aqi=no&alerts=no', {mode: "cors"});
     const data = await weather.json();
 
     document.querySelector('.condition').textContent = data['current']['condition']['text'];
